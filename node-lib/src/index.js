2c2777bfcd38f0eaad9a7581d81600493274d58b
jvtrfortunato <joaovitor.fortunato@hotmail.com>
2025-07-21 19:20:25 -0300

    ⚠️ feat: trata erros

--- node-lib/src/index.js
+++ node-lib/src/index.js
@@ -1,24 +1,20 @@
 const fs = require('fs');
+const trataErros = require('./erros/funcoesErro');
 
 const caminhoArquivo = process.argv;  
 const link = caminhoArquivo[2];
 
-fs.readFile(link, 'utf-8', (erro, texto) => {
-    quebraEmParagrafos(texto);
-    // verificaPalavrasDuplicadas(texto);
+fs.readFile(link, 'utf-8', (erro, texto) => { 
+    try {
+        if (erro) throw erro
+        contaPalavras(texto);
+    } catch(erro) {
+        trataErros(erro);
+    }
 })
 
-// criar um array com as palavras
-// contar as ocorrências 
-// montar um objeto com o resultado
-
-// {
-//     "web": 5,
-//     "computador": 4
-// }
-
-function quebraEmParagrafos(texto) {
-    const paragrafos = texto.toLowerCase().split('\n')
+function contaPalavras(texto) {
+    const paragrafos = extraiParagrafos(texto)
     const contagem = paragrafos.flatMap((paragrafo) => {
         if(!paragrafo) return [];
         return verificaPalavrasDuplicadas(paragrafo);
@@ -26,8 +22,9 @@ function quebraEmParagrafos(texto) {
     console.log(contagem);
 }
 
-// [1, 2 [3, 4]]
-// [1, 2, 3, 4]
+function extraiParagrafos(texto) {
+    return texto.toLowerCase().split('\n')
+}
 
 function limpaPalavras(palavra) {
     return palavra.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g, '');
@@ -36,7 +33,6 @@ function limpaPalavras(palavra) {
 function verificaPalavrasDuplicadas(texto) {
     const listaPalavras = texto.split(' ');
     const resultado = {};
-    // objeto[propriedade] = valor;
     listaPalavras.forEach(palavra => {
         if (palavra.length >= 3) {
             const palavraLimpa = limpaPalavras(palavra);
