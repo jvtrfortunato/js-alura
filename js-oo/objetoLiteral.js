bd4cc3a61f02371e0b40289e8626499a34ca7e81
jvtrfortunato <joaovitor.fortunato@hotmail.com>
2025-07-28 12:04:40 -0300

    📐 Orientação a Objetos - funções e protótipos

--- js-oo/objetoLiteral.js
+++ js-oo/objetoLiteral.js
@@ -8,3 +8,15 @@ const user = {
         console.log(this.nome, this.email);
     }
 }
+
+// user.exibirInfos();
+
+// const exibir = user.exibirInfos
+// exibir()
+
+const exibir = function() {
+    console.log(this.nome, this.email);
+}
+
+const exibirNome = exibir.bind(user);
+exibirNome();
